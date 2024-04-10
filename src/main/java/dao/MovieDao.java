1da3411c756526dab0b8f25152e04787d576fdfd
Nayana2518 <nayana2518@gmail.com>
2024-04-10 10:28:25 +0530

    Fetch Movies done

---
+++ src/main/java/dao/MovieDao.java
@@ -0,0 +1,26 @@
+package dao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+import java.util.List;
+import dto.Movie;
+
+public class MovieDao {
+	
+	EntityManagerFactory factory=Persistence.createEntityManagerFactory("nayana");
+	EntityManager manager=factory.createEntityManager();
+	EntityTransaction transaction=manager.getTransaction();
+
+	public void saveMovie(Movie movie) {
+		transaction.begin();
+		manager.persist(movie);
+		transaction.commit();
+	}
+	
+	public List<Movie> fetchMovies(){
+		return manager.createNativeQuery("select x from movie x").getResultList();
+	}
+}
